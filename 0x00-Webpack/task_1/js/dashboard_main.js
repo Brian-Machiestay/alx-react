b2dbbf72506f08c8e5d86997c614b1c4016b1d0e
Brian-Machiestay <brianmachiestay@gmail.com>
2023-06-04 22:05:33 +0000

    add elements in given order

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,12 +2,11 @@ import * as jQuery from 'jquery';
 import { debounce } from 'lodash';
 
 jQuery(document).ready(function($) {
-
-    $('body').prepend('<p>Copyright - Holberton School</p>');
-    $('body').prepend('<p id="count"></p>');
-    $('body').prepend('<button>Click here to get started</button>');
-    $('body').prepend('<p>Dashboard data for the students</p>');
-    $('body').prepend('<p>Holberton Dashboard</p>');
+    $('<p>Holberton Dashboard</p>').insertBefore('script');
+    $('<p>Dashboard data for the students</p>').insertBefore('script');
+    $('<button>Click here to get started</button>').insertBefore('script');
+    $('<p id="count"></p>').insertBefore('script');
+    $('<p>Copyright - Holberton School</p>').insertBefore('script');
 
     let count = 0;
     function updateCounter(e) {
