e68b2001c1ccd45a69468dcefa92cd51274299ed
Brian-Machiestay <brianmachiestay@gmail.com>
2023-06-08 12:21:09 +0000

    load css and images through webpack

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import * as jQuery from 'jquery';
+import { debounce } from 'lodash';
+import '../css/main.css';
+
+jQuery(document).ready(function($) {
+    $('body').prepend('<div id="logo"></div>');
+    $('<p>Holberton Dashboard</p>').insertBefore('script');
+    $('<p>Dashboard data for the students</p>').insertBefore('script');
+    $('<button>Click here to get started</button>').insertBefore('script');
+    $('<p id="count"></p>').insertBefore('script');
+    $('<p>Copyright - Holberton School</p>').insertBefore('script');
+
+    let count = 0;
+    function updateCounter() {
+	count += 1;
+	$('#count').text(`${count} clicks on the button`);
+    }
+
+    $('button').click(debounce(updateCounter));
+})
