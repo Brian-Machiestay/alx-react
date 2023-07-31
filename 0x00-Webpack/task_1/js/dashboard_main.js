c0be7281dc5cd2f30b0337ba4781de11247d246b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-07-31 16:04:24 +0000

    import script in body

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,20 +1,20 @@
 import * as jQuery from 'jquery';
 import _ from 'lodash';
 
-jQuery(document).ready(function($) {
-    $('<p>Holberton Dashboard</p>').insertBefore('script');
-    $('<p>Dashboard data for the students</p>').insertBefore('script');
-    $('<button>Click here to get started</button>').insertBefore('script');
-    $('<p id="count"></p>').insertBefore('script');
-    $('<p>Copyright - Holberton School</p>').insertBefore('script');
 
-    let count = 0;
-    function updateCounter() {
-	count += 1;
-	$('#count').text(`${count} clicks on the button`);
-    }
+$('<p>Holberton Dashboard</p>').insertBefore('script');
+$('<p>Dashboard data for the students</p>').insertBefore('script');
+$('<button>Click here to get started</button>').insertBefore('script');
+$('<p id="count"></p>').insertBefore('script');
+$('<p>Copyright - Holberton School</p>').insertBefore('script');
 
-    const debounced_fun = _.debounce(updateCounter);
+let count = 0;
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
 
-    $('button').click(debounced_fun);
-})
+const debounced_fun = _.debounce(updateCounter);
+
+$('button').click(debounced_fun);
+
